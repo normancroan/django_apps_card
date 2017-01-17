311fd96cff1e60cf5eca5686186d9bb360346ea1
Norman Croan <ncroan@gmail.com>
2017-01-17 00:35:11 +0000

    pulling card info from database

--- urls.py
+++ urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	# ex: /card/4
-	url(r'^(?P<card_id>[0-9]+)$', views.detail, name='detail')
+	url(r'^(?P<card_id>[0-9]+)/$', views.detail, name='detail')
 ]
--- views.py
+++ views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Chatter
 # Create your views here.
 
 def index(request):
 	latest_chatter_list = Chatter.objects.order_by('-pub_date')[:5]
-	output = ', '.join([c.chatter_content for c in latest_chatter_list])
-	return HttpResponse(output)
+	#output = ', '.join([c.chatter_content for c in latest_chatter_list])
+	#return HttpResponse(output)
+	template = loader.get_template('card/index.html')
+	context = {
+		'latest_chatter_list': latest_chatter_list,
+	}
+	return HttpResponse(template.render(context, request))
 
 def detail(request, card_id):
 	response = "Here you'll find the details for card %s."
