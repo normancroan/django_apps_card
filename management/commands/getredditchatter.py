1ee3b07a56e39105de878dd1fbbada0492fea4e1
Norman Croan <ncroan@gmail.com>
2017-01-18 18:03:22 -0500

    connecting to db for bot

--- management/commands/getredditchatter.py
+++ management/commands/getredditchatter.py
@@ -1,9 +1,22 @@
 from django.core.management.base import BaseCommand, CommandError
-from card.models import RedditBot
+from card.models import RedditBot, Chatter, EternalCard
+import praw
 
 class Command(BaseCommand):
 	help = 'kicks off the Reddit bot and saved matches to our Chatter database'
 
 	def handle(self, *args, **options):
-		new_entry = RedditBot(subreddit="eternalguru", matchkey="sandstorm titan")
-		new_entry.save()
+		#new_entry = RedditBot(subreddit="eternalguru", matchkey="sandstorm titan")
+		#new_entry.save()
+
+		subreddit = reddit.subreddit('eternalcardgame')
+		#stage 'observed' list
+		observed = []
+		for observation in RedditBot.objects.all():
+			observed.append(observation.matchkey)
+		#stage phrase list
+		phraseList = []
+		for card in EternalCard.objects.all():
+			for alias in card.aliases:
+				phraseList.append(alias)
+				self.stdout.write('added alias: ',alias)
