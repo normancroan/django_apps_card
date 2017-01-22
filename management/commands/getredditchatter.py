e96990009d1e512f1dc8580999759e7470c54085
Norman Croan <ncroan@gmail.com>
2017-01-22 15:19:24 -0500

    updated getorcreate

--- management/commands/getredditchatter.py
+++ management/commands/getredditchatter.py
@@ -19,5 +19,5 @@ class Command(BaseCommand):
 
 		for item in observed:
 			#print(item)
-			new_entry = RedditBot.get_or_create(subreddit="eternalguru", matchkey=item)
+			new_entry = RedditBot.objects.get_or_create(subreddit="eternalguru", matchkey=item)
 			#new_entry.save()
