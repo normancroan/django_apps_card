d9c3645cb8bbcaf072155010101808176f0fa43e
Norman Croan <ncroan@gmail.com>
2017-01-22 18:36:53 -0500

    setup chatter url

--- views.py
+++ views.py
@@ -19,3 +19,12 @@ def detail(request, card_id):
 	response = "Here you'll find the details for card %s."
 	return HttpResponse(response % card_id)
 
+def chatter(request):
+	latest_chatter_list = Chatter.objects.order_by('-pub_date')[:5]
+	#output = ', '.join([c.chatter_content for c in latest_chatter_list])
+	#return HttpResponse(output)
+	template = loader.get_template('chatter/index.html')
+	context = {
+		'latest_chatter_list': latest_chatter_list,
+	}
+	return HttpResponse(template.render(context, request))
