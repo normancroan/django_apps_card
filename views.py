28aa7d61722e50696808f73cdf0fd29110e6d17c
Norman Croan <ncroan@gmail.com>
2017-01-22 23:00:25 -0500

    reverting card name changes

--- views.py
+++ views.py
@@ -10,9 +10,8 @@ def index(request):
 	context = {'latest_chatter_list': latest_chatter_list,}
 	return render(request, 'card/index.html', context)
 
-def detail(request, card_name):
-	name = card_name.replace('_',' ')
-	card = get_object_or_404(EternalCard, name=card_name)
+def detail(request, card_id):
+	card = get_object_or_404(EternalCard, num=card_id)
 	return render(request, 'card/detail.html', {'card': card})
 
 def chatter_detail(request, chatter_id):
