821666446ff89c927205bb688ab53a2f2ecf8edb
Norman Croan <ncroan@gmail.com>
2017-01-22 21:12:54 -0500

    testing render function in view

--- views.py
+++ views.py
@@ -7,13 +7,8 @@ from .models import Chatter
 
 def index(request):
 	latest_chatter_list = Chatter.objects.order_by('-pub_date')[:5]
-	#output = ', '.join([c.chatter_content for c in latest_chatter_list])
-	#return HttpResponse(output)
-	template = loader.get_template('card/index.html')
-	context = {
-		'latest_chatter_list': latest_chatter_list,
-	}
-	return HttpResponse(template.render(context, request))
+	context = {'latest_chatter_list': latest_chatter_list,}
+	return render(request, 'card/index.html', context)
 
 def detail(request, card_id):
 	response = "Here you'll find the details for card %s."
