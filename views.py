e732625255f3bcf17fa2f20610eb31aa3b4eb559
Norman Croan <ncroan@gmail.com>
2017-01-23 11:40:49 -0500

    change card view for card detail to use pk instead of num, cards have clashing num in case of different sets

--- views.py
+++ views.py
@@ -11,7 +11,7 @@ def index(request):
 	return render(request, 'card/index.html', context)
 
 def detail(request, card_id):
-	card = get_object_or_404(EternalCard, num=card_id)
+	card = get_object_or_404(EternalCard, pk=card_id)
 	return render(request, 'card/detail.html', {'card': card})
 
 def chatter_detail(request, chatter_id):
