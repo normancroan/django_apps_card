17d7b3445af6b34cfde8b7cd9c81fcb2f412acdd
Norman Croan <ncroan@gmail.com>
2017-01-23 16:42:17 -0500

    filter down to the card

--- views.py
+++ views.py
@@ -10,8 +10,7 @@ def index(request):
 
 def detail(request, card_name):
 	card = get_object_or_404(EternalCard, name__iexact=card_name.replace('_',' '))
-	#chatter = EternalCard.objects.annotate(chatter_count=Count('chatter'))
-	chatter = card.annotate(chatter_count=Count('chatter'))
+	chatter = EternalCard.objects.filter(pk=card.pk).annotate(chatter_count=Count('chatter'))
 	return render(request, 'card/detail.html', {'card': card, 'chatter': chatter})
 
 def detail_help(request):
