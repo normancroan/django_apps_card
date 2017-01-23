a863b5c4763cccd616349249beb0a19e89d74e86
Norman Croan <ncroan@gmail.com>
2017-01-22 21:43:51 -0500

    tabs...

--- management/commands/addcardstomodel.py
+++ management/commands/addcardstomodel.py
@@ -3,12 +3,12 @@ from card.models import RedditBot, Chatter, EternalCard
 import json
 
 class Command(BaseCommand):
-	help = 'adds cards to model from eternal json'
+    help = 'adds cards to model from eternal json'
 
-	def handle(self, *args, **options):
+    def handle(self, *args, **options):
         with open('eternal-0.91.json') as json_data:
             cards = json.load(json_data)
             for card in cards:
-    			print(card['name'])
-    			#new_entry = RedditBot.objects.get_or_create(subreddit="eternalguru", matchkey=item)
-    			#new_entry.save()
+                print(card['name'])
+                #new_entry = RedditBot.objects.get_or_create(subreddit="eternalguru", matchkey=item)
+                #new_entry.save()
