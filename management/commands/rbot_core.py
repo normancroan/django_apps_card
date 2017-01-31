37234a635f39971823c6b11a0a3413e672518464
Norman Croan <ncroan@gmail.com>
2017-01-30 20:30:10 -0500

    test get comments

--- management/commands/rbot_core.py
+++ management/commands/rbot_core.py
@@ -15,7 +15,7 @@ def setupBot(card,observedList):
 def getSubmissions(subreddit,card):
     print("getting submissions from: ",subreddit, 'for: ',card.name)
     for submission in subreddit.hot(limit=1):
-        print(submission.get_info(thing_id='t1_acx23'))
+        print(submission.get_comments(thing_id='t1_acx23'))
         submission.comments.replace_more(limit=0)
         parseSubmission(submission,card)
 
